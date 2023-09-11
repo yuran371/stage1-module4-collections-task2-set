c037bb57a3b28759aae1e0c62c26052bfbef0a5d
rosom <yuoran371@gmail.com>
2023-09-11 14:43:25 +0300

    Add finished project

--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -2,8 +2,19 @@ package com.epam.mjc.collections.set;
 
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+    	int lb = lowerBound;
+    	int ub = upperBound;
+    	TreeSet<Integer> treeSet = new TreeSet<Integer>();
+    	for (int i=0; i<sourceList.size(); i++) {
+    	int el = sourceList.get(i)*sourceList.get(i);
+    	if (lb<=el&&el<=ub) {
+    		treeSet.add(el);		
+    	}
     }
+		return treeSet;
+}
 }
