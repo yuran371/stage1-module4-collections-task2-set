b05ff37d3b4700a4f6c40c58db0c4621576b88da
rosom <yuoran371@gmail.com>
2023-09-11 15:02:38 +0300

    Add finished project

--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -1,9 +1,24 @@
 package com.epam.mjc.collections.set;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+    	
+    	HashSet<String> intersaction = new HashSet<String>(firstSet);
+    	intersaction.retainAll(secondSet);
+    	intersaction.removeAll(thirdSet);
+    	HashSet<String> intersaction2 = new HashSet<String>(thirdSet);
+    	intersaction2.removeAll(firstSet);
+    	intersaction2.removeAll(secondSet);
+    	intersaction.addAll(intersaction2);
+		return intersaction;
+    	
+    	
+
+    	
+    	
     }
 }
