0322f17e675ea5c565961f817b3e101115af795e
rosom <yuoran371@gmail.com>
2023-09-11 14:28:40 +0300

    Add finished project

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -2,8 +2,28 @@ package com.epam.mjc.collections.set;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+    	HashSet<Integer> si = new HashSet<Integer>();
+    	for (int i=0; i<sourceList.size(); i++) {
+    		int intFromSourceList = sourceList.get(i);
+    	if (intFromSourceList%2==0) {
+			while (intFromSourceList>=1) {
+				if(intFromSourceList%2==0) {
+					si.add(intFromSourceList);
+					intFromSourceList/=2;
+				} else {
+					si.add(intFromSourceList);
+					break;
+				}
+			}
+    		} else {
+				si.add(intFromSourceList);
+				si.add(intFromSourceList*2);
+    		}
+    	}
+    	return si;
     }
 }
